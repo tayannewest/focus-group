f410c0f4951d4d887625e062d83524f3e573b89a
Tay West <tayanne.west@gmail.com>
2021-11-08 20:43:34 -0500

    add show function to profile

--- controllers/profiles.js
+++ controllers/profiles.js
@@ -14,6 +14,27 @@ function index(req, res) {
   })
 }
 
+function show(req, res) {
+  Profile.findById(req.params.id)
+  .then((profile) => {
+    Profile.findById(req.user.profile._id)
+    .then(self => {
+      const isSelf = self._id.equals(profile._id)
+      res.render("profiles/show", {
+        title: `${profile.name}'s Profile`,
+        profile,
+        self,
+        isSelf
+      })
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/ideas")
+  })
+}
+
 export {
-  index
+  index,
+  show
 }
--- routes/profiles.js
+++ routes/profiles.js
@@ -1,11 +1,13 @@
 import { Router } from 'express'
-import { isLoggedIn } from '../middleware/middleware.js'
 import * as profilesCtrl from '../controllers/profiles.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // localhost:3000/profiles - GET
-router.get("/", isLoggedIn, profilesCtrl.index)
+router.get("/", profilesCtrl.index)
+// localhost:3000/profiles/:id - GET
+router.get("/:id", profilesCtrl.show)
 
 export {
   router
