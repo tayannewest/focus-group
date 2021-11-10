071716b42ac05d1e93c957b74d04911da98a7828
Tay West <tayanne.west@gmail.com>
2021-11-10 09:52:53 -0500

    add UI for profile idea count

--- models/profile.js
+++ models/profile.js
@@ -4,14 +4,8 @@ import mongoose from "mongoose"
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
-  ideas: {
-    type: Number,
-    min: 0,
-  },
-  reviews: {
-    type: Number,
-    min: 0,
-  },
+  ideas: [],
+  reviews: [],
 }, {
   timestamps: true
 })
